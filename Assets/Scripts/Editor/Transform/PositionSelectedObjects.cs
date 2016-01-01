1212c904d1fe612ef6f1e757e99fc4c2d38ea0e3
Momin Khan <foolmoron@gmail.com>
2016-01-01 18:36:54 -0500

    Main menu for selecting all 10 games

---
+++ Assets/Scripts/Editor/Transform/PositionSelectedObjects.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class PositionSelectedObjects : ScriptableWizard {
+    static Transform[] objectsToPosition = { };
+    static Vector3 initialPosition = Vector3.zero;
+    static Vector3 positionStep = Vector3.zero;
+    static bool local = true;
+
+    public Transform[] ObjectsToPosition = { };
+    public Vector3 InitialPosition = Vector3.zero;
+    public Vector3 PositionStep = Vector3.zero;
+    public bool Local = true;
+
+    [MenuItem("Transform/Position Selected Objects...")]
+    static void CreateWizard() {
+        DisplayWizard("Position Selected Objects", typeof(PositionSelectedObjects), "Done");
+    }
+
+    public PositionSelectedObjects() {
+        ObjectsToPosition = Selection.GetTransforms(SelectionMode.TopLevel | SelectionMode.OnlyUserModifiable);
+        InitialPosition = initialPosition;
+        PositionStep = positionStep;
+        Local = local;
+
+        Undo.RecordObjects(ObjectsToPosition, "Position Selected Objects");
+    }
+
+    void OnWizardUpdate() {
+        objectsToPosition = ObjectsToPosition;
+        initialPosition = InitialPosition;
+        positionStep = PositionStep;
+        local = Local;
+
+        // process results in update for better UX
+        for (int i = 0; i < objectsToPosition.Length; i++) {
+            var obj = objectsToPosition[i];
+            if (local) {
+                obj.localPosition = initialPosition + positionStep * i;
+            } else {
+                obj.position = initialPosition + positionStep * i;
+            }
+        }
+    }
+
+    void OnWizardCreate() {
+    }
+}
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+    
+    public void LoadScene(string scene) {
+        SceneManager.LoadScene(scene);
+    }
+}
