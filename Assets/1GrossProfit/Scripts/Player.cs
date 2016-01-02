eced801c6b52e5e4a4b87a8e82729fa57bb30098
Momin Khan <foolmoron@gmail.com>
2016-01-01 19:47:05 -0500

    Basic input for Gross Profit

---
+++ Assets/1GrossProfit/Scripts/Player.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    [Range(0, 10)]
+    public float Speed = 4;
+    [Range(0, 1)]
+    public float Encumbrance;
+
+    public void Update() {
+        var input = Vector2.zero;
+        // get input 
+        {
+            // mouse click input first
+            if (Input.GetMouseButton(0)) {
+                var vectorToMouse = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position).to2();
+                if (vectorToMouse.magnitude > 0.2f) {
+                    input = vectorToMouse.normalized;
+                }
+            } else {
+                // then keyboard wasd/arrows input
+                if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) {
+                    input.y = 1;
+                } else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) {
+                    input.y = -1;
+                }
+                if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
+                    input.x = 1;
+                } else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
+                    input.x = -1;
+                }
+                input = input.normalized;
+            }
+        }
+        // move based on input
+        {
+            var movement = Speed * (1 - Encumbrance) * Time.deltaTime * input;
+            transform.position += movement.to3();
+        }
+    }
+}
