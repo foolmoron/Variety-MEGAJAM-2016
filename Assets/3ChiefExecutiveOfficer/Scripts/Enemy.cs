01d6a2b8833e0fc5c7044080bdd08a88ac7070b3
Momin Khan <foolmoron@gmail.com>
2016-01-03 16:56:04 -0500

    Better enemy color and movement

--- Assets/3ChiefExecutiveOfficer/Scripts/Enemy.cs
+++ Assets/3ChiefExecutiveOfficer/Scripts/Enemy.cs
@@ -8,10 +8,19 @@ public class Enemy : MonoBehaviour {
     [Range(0, 10)]
     public float Speed = 1;
 
+    void Start() {
+        GetComponent<SpriteRenderer>().color = new HSBColor(Random.value, 0.5f, 1f).ToColor();
+    }
+
     void Update() {
-        // approach target
+        // approach target manhattan style
         {
-            transform.position = Vector3.MoveTowards(transform.position, Target.position, Speed * Time.deltaTime);
+            var directionToTarget = (Target.position - transform.position).to2().normalized;
+            if (Mathf.Abs(directionToTarget.x) > Mathf.Abs(directionToTarget.y)) {
+                transform.position = transform.position.plusX(Mathf.Sign(directionToTarget.x) * Speed * Time.deltaTime);
+            } else {
+                transform.position = transform.position.plusY(Mathf.Sign(directionToTarget.y) * Speed * Time.deltaTime);
+            }
         }
     }
 
