113cab1106eb4cb7b7adbd9d9522c51590ceb325
Momin Khan <foolmoron@gmail.com>
2016-01-03 17:07:04 -0500

    Can kill and bloody stuff

--- Assets/3ChiefExecutiveOfficer/Scripts/Enemy.cs
+++ Assets/3ChiefExecutiveOfficer/Scripts/Enemy.cs
@@ -8,6 +8,11 @@ public class Enemy : MonoBehaviour {
     [Range(0, 10)]
     public float Speed = 1;
 
+    public GameObject BloodPrefab;
+    public int BloodsToSpawn = 3;
+    [Range(0, 1)]
+    public float BloodDistance = 0.1f;
+
     void Start() {
         GetComponent<SpriteRenderer>().color = new HSBColor(Random.value, 0.5f, 1f).ToColor();
     }
@@ -29,6 +34,14 @@ public class Enemy : MonoBehaviour {
         if (bullet) {
             Destroy(bullet.gameObject);
             Destroy(gameObject);
+
+            // bloods
+            for (int i = 0; i < BloodsToSpawn; i++) {
+                var blood = Instantiate(BloodPrefab);
+                blood.GetComponent<SpriteRenderer>().color = GetComponent<SpriteRenderer>().color.withAlpha(0.5f);
+                blood.transform.position = transform.position.to2() + Random.insideUnitCircle * BloodDistance;
+                blood.transform.Rotate(0, 0, Random.value * 360f);
+            }
         }
 
         var player = collision.GetComponent<TwinStick>();
