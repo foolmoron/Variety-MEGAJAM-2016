ec1e02de1f627f8bcb76212a3dd1da17b226fd42
Momin Khan <foolmoron@gmail.com>
2016-01-02 21:25:17 -0500

    Added escape to return to main menu

---
+++ Assets/Scripts/Returner.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Returner : MonoBehaviour {
+
+    [Range(0, 5)]
+    public float TimeToHold = 3;
+    float timeHeld;
+    GameObject stuff;
+
+    Shaker shaker;
+    float initialShake;
+    
+    void Start() {
+        // check if another exists, and delete self if necessary
+        if (FindObjectOfType<Returner>() != this) {
+            Destroy(gameObject);
+            return;
+        }
+        // keep alive always
+        DontDestroyOnLoad(gameObject);
+        // init stuff
+        stuff = transform.FindChild("Stuff").gameObject;
+        shaker = GetComponentInChildren<Shaker>();
+        initialShake = shaker.Strength;
+        stuff.SetActive(false);
+    }
+    
+    void Update() {
+        // don't do anything on main menu
+        if (SceneManager.GetActiveScene().name == "MainMenu") {
+            return;
+        }
+
+        if (Input.GetKey(KeyCode.Escape)) {
+            stuff.SetActive(true);
+            timeHeld += Time.deltaTime;
+            if (timeHeld >= TimeToHold) {
+                stuff.SetActive(false);
+                SceneManager.LoadScene("MainMenu");
+            }
+            shaker.Strength = initialShake * (timeHeld / TimeToHold);
+        } else {
+            stuff.SetActive(false);
+            timeHeld = 0;
+        }
+    }
+}
