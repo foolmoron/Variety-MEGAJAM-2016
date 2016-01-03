45fbfc3d9f77c784cbd0ce04d24a20e4ddc367f1
Momin Khan <foolmoron@gmail.com>
2016-01-03 16:51:16 -0500

    Spawning enemies

--- Assets/3ChiefExecutiveOfficer/Scripts/Enemy.cs
+++ Assets/3ChiefExecutiveOfficer/Scripts/Enemy.cs
@@ -4,7 +4,18 @@ using System.Collections.Generic;
 
 public class Enemy : MonoBehaviour {
 
-    public void OnTriggerEnter2D(Collider2D collision) {
+    public Transform Target;
+    [Range(0, 10)]
+    public float Speed = 1;
+
+    void Update() {
+        // approach target
+        {
+            transform.position = Vector3.MoveTowards(transform.position, Target.position, Speed * Time.deltaTime);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision) {
         var bullet = collision.GetComponent<Bullet>();
         if (bullet) {
             Destroy(bullet.gameObject);
---
+++ Assets/3ChiefExecutiveOfficer/Scripts/EnemySpawner.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EnemySpawner : MonoBehaviour {
+
+    [Range(0, 10)]
+    public float SpawnInterval = 4f;
+    [Range(-1, 1)]
+    public float SpawnIntervalVelocity = -0.05f;
+    float spawnTime = float.PositiveInfinity;
+
+    public Transform Target;
+    public GameObject SpawnPrefab;
+    [Range(0, 5)]
+    public float SpawnMinDistance = 1f;
+    [Range(0, 10)]
+    public float SpawnMaxDistance = 5f;
+    [Range(0, 100)]
+    public int InitialSpawn = 10;
+
+    void Start() {
+        for (int i = 0; i < InitialSpawn; i++) {
+            Spawn();
+        }
+    }
+
+    public void Spawn() {
+        var enemy = Instantiate(SpawnPrefab).GetComponent<Enemy>();
+        enemy.Target = Target;
+
+        var randomAngle = Random.value * Mathf.PI * 2;
+        var direction = new Vector2(Mathf.Cos(randomAngle), Mathf.Sin(randomAngle));
+        var position = transform.position + direction.to3() * Mathf.Lerp(SpawnMinDistance, SpawnMaxDistance, Random.value);
+        enemy.transform.position = position;
+    }
+
+    void Update() {
+        // spawn interval velocity
+        {
+            SpawnInterval += SpawnIntervalVelocity * Time.deltaTime;
+        }
+        // spawn
+        {
+            spawnTime += Time.deltaTime;
+            if (spawnTime >= SpawnInterval) {
+                Spawn();
+                spawnTime = 0;
+            }
+        }
+    }
+}
