d92ad270ce3489e5157f80d7218edcb3769cc630
Momin Khan <foolmoron@gmail.com>
2016-01-03 16:37:37 -0500

    Bullets die out of bounds and kill enemies

---
+++ Assets/3ChiefExecutiveOfficer/Scripts/BulletKiller.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BulletKiller : MonoBehaviour {
+
+    public void OnTriggerEnter2D(Collider2D collision) {
+        var bullet = collision.GetComponent<Bullet>();
+        if (bullet) {
+            Destroy(bullet.gameObject);
+        }
+    }
+}
---
+++ Assets/3ChiefExecutiveOfficer/Scripts/Enemy.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Enemy : MonoBehaviour {
+
+    public void OnTriggerEnter2D(Collider2D collision) {
+        var bullet = collision.GetComponent<Bullet>();
+        if (bullet) {
+            Destroy(bullet.gameObject);
+            Destroy(gameObject);
+        }
+
+        var player = collision.GetComponent<TwinStick>();
+        if (player) {
+            // TODO: gameover
+            Destroy(gameObject);
+        }
+    }
+}
