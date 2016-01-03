bedf0933c39fd02b5d056492b8617ce75e8734de
Momin Khan <foolmoron@gmail.com>
2016-01-03 16:23:21 -0500

    Starting day 3 game

---
+++ Assets/3ChiefExecutiveOfficer/Scripts/TwinStick.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TwinStick : MonoBehaviour {
+
+    [Range(0, 10)]
+    public float Speed = 2;
+
+    public GameObject BulletPrefab;
+    
+    void Start() {
+    
+    }
+    
+    void Update() {
+        // move based on WASD
+        {
+            var direction = Vector2.zero;
+            if (Input.GetKey(KeyCode.W)) direction.y = 1;
+            if (Input.GetKey(KeyCode.S)) direction.y = -1;
+            if (Input.GetKey(KeyCode.D)) direction.x = 1;
+            if (Input.GetKey(KeyCode.A)) direction.x = -1;
+            transform.position += direction.normalized.to3() * Speed * Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/Scripts/SpriteSwapper.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteSwapper : MonoBehaviour {
+
+    public Sprite Sprite1;
+    [Range(0, 3)]
+    public float Sprite1Time;
+    public Sprite Sprite2;
+    [Range(0, 3)]
+    public float Sprite2Time;
+    bool currently1 = true;
+    float time;
+
+    SpriteRenderer sprite;
+
+    void Start() {
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    void Update() {
+        time += Time.deltaTime;
+        if ((currently1 && time >= Sprite1Time) || (!currently1 && time >= Sprite2Time)) {
+            currently1 = !currently1;
+            sprite.sprite = currently1 ? Sprite1 : Sprite2;
+            time = 0;
+        }
+    }
+}
