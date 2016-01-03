d8b98870d631bc6bb304033adbca6c25fb511d1f
Momin Khan <foolmoron@gmail.com>
2016-01-03 16:32:46 -0500

    Shooting

---
+++ Assets/3ChiefExecutiveOfficer/Scripts/Bullet.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Bullet : MonoBehaviour {
+
+    [Range(0, 30)]
+    public float Speed = 2;
+
+    void Update() {
+        // move
+        {
+            transform.Translate(new Vector3(Speed * Time.deltaTime, 0, 0));
+        }
+    }
+}
--- Assets/3ChiefExecutiveOfficer/Scripts/TwinStick.cs
+++ Assets/3ChiefExecutiveOfficer/Scripts/TwinStick.cs
@@ -7,6 +7,9 @@ public class TwinStick : MonoBehaviour {
     [Range(0, 10)]
     public float Speed = 2;
 
+    [Range(0, 3)]
+    public float ShootInterval = 0.5f;
+    float shootTime;
     public GameObject BulletPrefab;
     
     void Start() {
@@ -22,6 +25,21 @@ public class TwinStick : MonoBehaviour {
             if (Input.GetKey(KeyCode.D)) direction.x = 1;
             if (Input.GetKey(KeyCode.A)) direction.x = -1;
             transform.position += direction.normalized.to3() * Speed * Time.deltaTime;
+        }
+        // shoot based on IJKL
+        {
+            var direction = Vector2.zero;
+            if (Input.GetKey(KeyCode.I)) direction.y = 1;
+            if (Input.GetKey(KeyCode.K)) direction.y = -1;
+            if (Input.GetKey(KeyCode.L)) direction.x = 1;
+            if (Input.GetKey(KeyCode.J)) direction.x = -1;
+
+            shootTime += Time.deltaTime;
+            if (direction != Vector2.zero && shootTime >= ShootInterval) {
+                var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+                var bullet = (GameObject)Instantiate(BulletPrefab, transform.position.plusZ(-0.1f), Quaternion.Euler(0, 0, angle));
+                shootTime = 0;
+            }
         }
     }
 }
