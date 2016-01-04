358d016bd9895e61ce3040bda95b02605af0f63f
Momin Khan <foolmoron@gmail.com>
2016-01-04 18:26:32 -0500

    Stacking

---
+++ Assets/4FirstInFirstOut/Scripts/LeftRightMover.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LeftRightMover : MonoBehaviour {
+
+    [Range(0, 10)]
+    public float Speed = 5;
+
+    public float MinX = -2.5f;
+    public float MaxX = 2.5f;
+    public bool MovingRight;
+
+    void Update() {
+        // move back and forth
+        {
+            if (MovingRight) {
+                var newX = Mathf.MoveTowards(transform.position.x, MaxX, Speed * Time.deltaTime);
+                transform.position = transform.position.withX(newX);
+                if (newX >= MaxX) {
+                    MovingRight = false;
+                }
+            } else {
+                var newX = Mathf.MoveTowards(transform.position.x, MinX, Speed * Time.deltaTime);
+                transform.position = transform.position.withX(newX);
+                if (newX <= MinX) {
+                    MovingRight = true;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/4FirstInFirstOut/Scripts/StackDropper.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StackDropper : MonoBehaviour {
+
+    public GameObject DropPrefab;
+
+    SpriteRenderer sprite;
+    
+    void Start() {
+        sprite = GetComponent<SpriteRenderer>();
+    }
+    void Update() {
+        // drop when clicked
+        {
+            if (Input.GetMouseButtonDown(0)) {
+                var drop = Instantiate(DropPrefab);
+                drop.transform.position = transform.position;
+                drop.transform.rotation = transform.rotation;
+                drop.transform.localScale = transform.localScale;
+                var dropSprite = drop.GetComponent<SpriteRenderer>();
+                dropSprite.color = sprite.color;
+            }
+        }
+    }
+}
