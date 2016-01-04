32987cedd4543048817b2b59d8cbf656264bfe89
Momin Khan <foolmoron@gmail.com>
2016-01-04 18:31:47 -0500

    colorful

--- Assets/4FirstInFirstOut/Scripts/StackDropper.cs
+++ Assets/4FirstInFirstOut/Scripts/StackDropper.cs
@@ -3,14 +3,24 @@ using System.Collections;
 
 public class StackDropper : MonoBehaviour {
 
+    [Range(0, 5)]
+    public float HueSpeed;
+
     public GameObject DropPrefab;
 
+    HSBColor currentColor;
     SpriteRenderer sprite;
     
     void Start() {
         sprite = GetComponent<SpriteRenderer>();
+        currentColor = HSBColor.FromColor(sprite.color);
     }
     void Update() {
+        // rotate hue
+        {
+            currentColor.h = (currentColor.h + HueSpeed * Time.deltaTime) % 1;
+            sprite.color = currentColor.ToColor();
+        }
         // drop when clicked
         {
             if (Input.GetMouseButtonDown(0)) {
@@ -19,7 +29,7 @@ public class StackDropper : MonoBehaviour {
                 drop.transform.rotation = transform.rotation;
                 drop.transform.localScale = transform.localScale;
                 var dropSprite = drop.GetComponent<SpriteRenderer>();
-                dropSprite.color = sprite.color;
+                dropSprite.color = sprite.color.withAlpha(1);
             }
         }
     }
