7ccb5f32043d1a4ec0bd3240ae3131f9d1af1593
Momin Khan <foolmoron@gmail.com>
2016-01-04 19:15:30 -0500

    Basic tipped logic

--- Assets/4FirstInFirstOut/Scripts/Stack.cs
+++ Assets/4FirstInFirstOut/Scripts/Stack.cs
@@ -5,8 +5,18 @@ public class Stack : MonoBehaviour {
 
     public FollowHeight FollowHeight;
     public bool IsAtTop;
+    public float HeightLanded = float.NegativeInfinity;
 
     void Start() {
+    }
+
+    void Update() {
+        // check if the stack tipped
+        {
+            if (transform.position.y < (HeightLanded - 0.1f)) {
+                Debug.Log("TIPPED");
+            }
+        }
     }
     
     public void OnCollisionEnter2D(Collision2D collision) {
@@ -14,6 +24,7 @@ public class Stack : MonoBehaviour {
         if (stack && stack.IsAtTop) {
             stack.IsAtTop = false;
             IsAtTop = true;
+            HeightLanded = transform.position.y;
             FollowHeight.Height = transform.position.y;
         }
     }
