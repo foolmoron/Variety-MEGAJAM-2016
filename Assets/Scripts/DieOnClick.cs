ab4065f0e271af156481bdd050fb880016fb85da
Momin Khan <foolmoron@gmail.com>
2016-01-04 19:32:11 -0500

    Added instructions

---
+++ Assets/Scripts/DieOnClick.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DieOnClick : MonoBehaviour {
+    
+    void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            Destroy(gameObject);
+        }
+    }
+}
