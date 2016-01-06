33bacd98ecbf3485e19f7259b81d20054334d53c
Momin Khan <foolmoron@gmail.com>
2016-01-05 21:11:21 -0500

    Starting game 5 with typing

---
+++ Assets/5ForeignExchange/Scripts/ChatInput.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChatInput : MonoBehaviour {
+    
+    [Range(1, 200)]
+    public int MaxInputLength = 6;
+
+    TextMesh input;
+
+    public AudioClip TypeSound;
+    public AudioClip SubmitSound;
+
+    void Start() {
+        input = transform.FindChild("Input").GetComponent<TextMesh>();
+        input.text = "";
+    }
+
+    void Update() {
+        // process inputs
+        {
+            var text = input.text;
+            var str = Input.inputString;
+            var ch = str.Length > 0 ? str[0] : 0;
+            if (Input.GetKeyDown(KeyCode.Backspace) || Input.GetKeyDown(KeyCode.Delete)) {
+                if (text.Length > 0) {
+                    text = text.Substring(0, text.Length - 1);
+                    AudioSource.PlayClipAtPoint(TypeSound, Vector3.zero);
+                }
+            } else if (Input.GetKeyDown(KeyCode.Return)) {
+                text = "";
+                AudioSource.PlayClipAtPoint(SubmitSound, Vector3.zero);
+            } else if (32 <= ch && ch <= 126 && text.Length < MaxInputLength) {
+                text += str;
+                AudioSource.PlayClipAtPoint(TypeSound, Vector3.zero);
+            }
+            input.text = text;
+        }
+    }
+}
