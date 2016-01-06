37d5433921493b867327ca161bfecc56686e1dfa
Momin Khan <foolmoron@gmail.com>
2016-01-05 23:14:55 -0500

    Random colors

---
+++ Assets/5ForeignExchange/Scripts/SetRandomColors.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetRandomColors : MonoBehaviour {
+
+    [Range(0, 1)]
+    public float Saturation = 0.5f;
+    public SpriteRenderer sprite1;
+    public TextMesh text1;
+    public TextMesh text1b;
+    public TextMesh text2;
+
+    void Start() {
+        var color = new HSBColor(Random.value, Saturation, 1);
+        sprite1.color = color.ToColor();
+        text1.color = color.ToColor();
+        text1b.color = color.ToColor();
+        var color2 = new HSBColor((color.h + 0.5f) % 1, Saturation, 1);
+        text2.color = color2.ToColor();
+    }
+}
