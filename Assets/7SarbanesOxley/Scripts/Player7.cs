214e78e3537a0672573daba09a4fa13019189ef6
Momin Khan <foolmoron@gmail.com>
2016-01-07 21:53:00 -0500

    Random rotation

--- Assets/7SarbanesOxley/Scripts/Player7.cs
+++ Assets/7SarbanesOxley/Scripts/Player7.cs
@@ -8,6 +8,11 @@ public class Player7 : MonoBehaviour {
     [Range(0, 30)]
     public float Speed = 5;
 
+    [Range(0, 1080)]
+    public float MinRandomRotation = 360;
+    [Range(0, 1080)]
+    public float MaxRandomRotation = 900;
+
     public KeyCode UpKey;
     public KeyCode LeftKey;
     public KeyCode RightKey;
@@ -22,6 +27,7 @@ public class Player7 : MonoBehaviour {
         var velocity = rigidbody.velocity;
         if (Input.GetKeyDown(UpKey)) {
             velocity.y += JumpForce;
+            rigidbody.angularVelocity = Mathf.Sign(Random.value - 0.5f) * Mathf.Lerp(MinRandomRotation, MaxRandomRotation, Random.value);
         }
         if (Input.GetKey(LeftKey)) {
             velocity.x = -Speed;
