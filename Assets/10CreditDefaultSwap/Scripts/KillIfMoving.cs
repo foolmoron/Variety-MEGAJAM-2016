11da291bcce8a64d8c080d37809f913d78459503
Momin Khan <foolmoron@gmail.com>
2016-01-10 18:10:32 -0500

    Spawning enemies and such

---
+++ Assets/10CreditDefaultSwap/Scripts/KillIfMoving.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillIfMoving : MonoBehaviour {
+
+    public bool LessThan;
+    public Vector3 Amount;
+
+    public void OnTriggerEnter2D(Collider2D collision) {
+        var moving = collision.GetComponent<Moving>();
+        if (moving) {
+            if (Amount.x != 0) {
+                if ((LessThan && moving.MovementPerSecond.x < Amount.x) || (!LessThan && moving.MovementPerSecond.x >= Amount.x)) {
+                    Destroy(moving.gameObject);
+                }
+            }
+            if (Amount.y != 0) {
+                if ((LessThan && moving.MovementPerSecond.y < Amount.y) || (!LessThan && moving.MovementPerSecond.y >= Amount.y)) {
+                    Destroy(moving.gameObject);
+                }
+            }
+            if (Amount.z != 0) {
+                if ((LessThan && moving.MovementPerSecond.z < Amount.z) || (!LessThan && moving.MovementPerSecond.z >= Amount.z)) {
+                    Destroy(moving.gameObject);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/10CreditDefaultSwap/Scripts/Spawner10.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner10 : MonoBehaviour {
+
+    public GameObject SpawnPrefab;
+
+    [Range(0, 1)]
+    public float Difficulty = 0;
+    [Range(0, 10)]
+    public float MinSecondsPerSpawn = 0.1f;
+    [Range(0, 10)]
+    public float MaxSecondsPerSpawn = 1f;
+    [Range(0, 10)]
+    public float MinSpawnSpeed = 0.5f;
+    [Range(0, 10)]
+    public float MaxSpawnSpeed = 5f;
+    [Range(0, 1)]
+    public float SpawnSpeedVariance = 0.25f;
+
+    Rect randomDroppingArea;
+    float timeSinceLastSpawn;
+
+    void Start() {
+        // get random dropping area from the box collider's bounds
+        {
+            var boxCollider = GetComponent<BoxCollider2D>();
+            randomDroppingArea = new Rect(boxCollider.offset - boxCollider.size / 2, boxCollider.size);
+        }
+    }
+    
+    void Update() {
+        var secondsPerSpawn = Mathf.Lerp(MaxSecondsPerSpawn, MinSecondsPerSpawn, Difficulty);
+        // drop new question
+        {
+            timeSinceLastSpawn += Time.deltaTime;
+            if (timeSinceLastSpawn >= secondsPerSpawn) { 
+                var randomOffset = new Vector2(Random.Range(randomDroppingArea.xMin, randomDroppingArea.xMax), Random.Range(randomDroppingArea.yMin, randomDroppingArea.yMax));
+                var spawn = (GameObject) Instantiate(SpawnPrefab, transform.position + randomOffset.to3(), Quaternion.identity);
+
+                var speed = Mathf.Lerp(MinSpawnSpeed, MaxSpawnSpeed, Difficulty) * (1 + (Random.value * SpawnSpeedVariance - SpawnSpeedVariance / 2));
+                spawn.GetComponent<Moving>().MovementPerSecond = spawn.GetComponent<Moving>().MovementPerSecond * speed;
+
+                spawn.GetComponent<GetColored10>().Inverted = Random.value <= 0.5f;
+                
+                timeSinceLastSpawn -= secondsPerSpawn;
+            }
+        }
+    }
+}
