e680b65f4e289af573bb6cae7ec85cd7f2d896ef
Momin Khan <foolmoron@gmail.com>
2016-01-09 20:20:37 -0500

    Fixed audio issue, turned off item pink sphere

--- Assets/8ChiefSecurityOfficer/Scripts/Staticer.cs
+++ Assets/8ChiefSecurityOfficer/Scripts/Staticer.cs
@@ -11,14 +11,30 @@ public class Staticer : MonoBehaviour {
     public Image Static1;
     public Image Static2;
 
+    ScaryItem[] items;
+
+    void Start() {
+        items = FindObjectsOfType<ScaryItem>();
+    }
+
     void Update() {
         if (OnFrames > 0) {
             Static1.color = Color.white;
             Static2.color = Color.white;
             OnFrames--;
+            for (int i = 0; i < items.Length; i++) {
+                if (items[i]) {
+                   items[i].gameObject.SetActive(false);   
+                }
+            }
         } else {
             Static1.color = Color.white.withAlpha(Amount);
             Static2.color = Color.white.withAlpha(Amount);
+            for (int i = 0; i < items.Length; i++) {
+                if (items[i]) {
+                    items[i].gameObject.SetActive(true);
+                }
+            }
         }
     }
 }
